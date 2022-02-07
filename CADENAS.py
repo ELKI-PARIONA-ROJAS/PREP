57669d6912ebd9e61e0f382667d60850c09ee411
elki pariona <elkiparionarojas@gmail.com>
2022-02-07 12:33:52 -0500

    Mi primer commit

---
+++ CADENAS.py
@@ -0,0 +1,18 @@
+
+# Mostrar solo la parte del numero de celular
+##nombre=input('Introduzca el telefo con el formato (+XX-XXXXXXXXX-XX): ')
+##indice_1=nombre.find('-')
+##indice_2=nombre.rfind('-')
+##
+##n1=nombre[indice_1+1:indice_2]
+##
+##print(n1)
+
+
+frase=input('Introducir una frase que llegue al corazón: ')#hola
+lista=[]
+for i in frase:
+    letra=frase[-1]#a , l
+    lista[-1]=letra
+    cadena=','.join(lista)
+print('la frase al volteado seria: ', cadena)
