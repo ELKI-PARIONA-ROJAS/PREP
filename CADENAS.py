8c19ae0ac7fb00916267a156b3a42aaaf455ce8d
elki pariona <elkiparionarojas@gmail.com>
2022-02-07 23:57:08 -0500

    Actualizado1

--- CADENAS.py
+++ CADENAS.py
@@ -8,11 +8,43 @@
 ##
 ##print(n1)
 
+# Voltear una cadena
+##frase=input('Introduzca una frase: ')
+##frase=frase[::-1]
+##print(frase)
 
-frase=input('Introducir una frase que llegue al bobo: ')#hola
-lista=[]
-for i in frase:
-    letra=frase[-1]#a , l
-    lista[-1]=letra
-    cadena=','.join(lista)
-print('la frase al volteado seria: ', cadena)
+#Convertir las vocales de una frase en mayusculas
+##cadena=input('Introducir la cadena: ')
+##cadena=cadena.lower()
+##cadena=cadena.replace('a','A')
+##cadena=cadena.replace('e','E')
+##cadena=cadena.replace('i','I')
+##cadena=cadena.replace('o','O')
+##cadena=cadena.replace('u','U')
+##print(cadena)
+
+#Corta y agrega un extensión en una cadena
+##correo=input('Introduce tu correo electrónico porfavor: ')
+##indice1=correo.find('@')
+##correo=correo[:indice1+1]
+##correo=correo+'.ceu.es'
+##print(correo)
+
+#Divide una cadena en función de un elemento
+##precio=input('Introducce el precio del producto con el siguiente formato: ')
+##indice1=precio.find('.')
+##entero=precio[:indice1]
+##flotante=precio[indice1+1:]
+##print('El precio del producto será de %s Soles con %s céntimos' % (entero,flotante))
+
+
+#Divide una cadena en varias partes
+##fecha=input('Introduce la fecha con el formata xx/xx/xx: ')
+##indice_dia=fecha.find('/')
+##dia=fecha[:indice_dia]
+##fecha=fecha[indice_dia+1:]
+##indice_mes=fecha.find('/')
+##mes=fecha[:indice_mes]
+##fecha=fecha[indice_mes+1:]
+##año=fecha
+##print('el dia de hoy es %s del mes de %s del año de %s' % (dia, mes, año))
