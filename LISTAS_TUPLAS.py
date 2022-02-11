f4e5bd27d8fee8bff9c03bcbdc6803cbef9e553f
elki pariona <elkiparionarojas@gmail.com>
2022-02-10 19:08:01 -0500

    terminado

--- LISTAS_TUPLAS.py
+++ LISTAS_TUPLAS.py
@@ -85,3 +85,106 @@
 #     elif i%3!=0:
 #         lista.append(letras[i])
 # print(lista)
+
+# palabra=input('Introduzca la palabra: ')
+# lista=[]
+# for i in palabra:
+#     lista.append(i)
+# if len(lista)%2==0:
+#     num=len(lista)/2
+#     num=int(num)
+#     p1=lista[:num]
+#     print(p1)
+#     p2=lista[num:]
+#     p2=p2[::-1]
+#     print(p2)
+#     if p1==p2:
+#         print('es una palabra palíndroma')
+#     else:
+#         print('No es una palabra palíndroma')
+# elif len(lista)%2!=0:
+#     num=(len(lista)-1)/2
+#     num=int(num)
+#     p1=lista[:num]
+#     print(p1)
+#     p2=lista[num+1:]
+#     p2=p2[::-1]
+#     print(p2)
+#     if p1==p2:
+#         print('es una palabra palíndroma')
+#     else:
+#         print('No es una palabra palíndroma')
+
+
+
+#Escribir un programa que pida al usuario una palabra y muestre por pantalla el número de veces 
+# que contiene cada vocal.
+
+# palabra=input('Escriba la palabra que desea: ')
+# palabra=palabra.lower()
+# lista=[]
+# for i in palabra:
+#     lista.append(i)
+# vocales=['a','e','i','o','u']
+# for i in vocales:
+#     print('El numero de veces que esta la vocal %s es de %s veces' % (i, lista.count(i)))
+
+
+
+#Escribir un programa que almacene en una lista los siguientes precios, 50, 75, 46, 22, 80, 
+# 65, 8, y muestre por pantalla el menor y el mayor de los precios.
+
+# lista=[50,75,46,22,80,65,8]
+# mayor=max(lista)
+# menor=min(lista)
+# print('El número mayor es %s y el número menor es %s' % (mayor, menor))
+
+
+
+# Escribir un programa que almacene los vectores (1,2,3) y (-1,0,2) en dos listas y muestre 
+# por pantalla su producto escalar.
+# l1=[1,2,3]
+# l2=[-1,0,2]
+# lista=[]
+# for i in range(3):
+#     n=l1[i]*l2[i]
+#     lista.append(n)
+# print(lista)
+
+
+
+# Escribir un programa que almacene las matrices en una lista y muestre por pantalla su producto.
+# Para representar matrices mediante listas usar listas anidadas, representando cada vector fila 
+# en una lista.
+# A=(1,2,5,8,5)
+# B=list(A)
+# print(B)
+
+
+
+#Escribir un programa que pregunte por una muestra de números, separados por comas, los guarde 
+# en una lista y muestre por pantalla su media y desviación típica.
+
+# lista=[]
+# x=True
+# while x:
+#     n=int(input('introduce un numero: '))
+#     lista.append(n)
+#     x=input('Desea continuar con la operación: ')
+#     if x=='si':
+#         pass
+#     elif x=='no':
+#         x=False
+# print(lista)
+# suma=0
+# contador=0
+# for i in lista:
+#     contador+=1
+#     suma=+i 
+# promedio=suma/contador
+# sumatoria=0
+# for i in lista:
+#     m=(i-promedio)**2
+#     sumatoria=+m
+# varianza=sumatoria/contador
+# print('El promedio es %s y la varianza es %s' % (promedio,varianza))
