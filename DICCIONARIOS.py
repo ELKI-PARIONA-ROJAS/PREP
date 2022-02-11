4554b2c1b8e8abd80cd6987cad1591eed3f7e1d6
elki pariona <elkiparionarojas@gmail.com>
2022-02-11 01:06:25 -0500

    final

---
+++ DICCIONARIOS.py
@@ -0,0 +1,31 @@
+# Escribir un programa que guarde en una variable el diccionario {'Euro':'€', 'Dollar':'$', 
+# 'Yen':'¥'}, pregunte al usuario por una divisa y muestre su símbolo o un mensaje de aviso si 
+# la divisa no está en el diccionario.
+
+# diccionario={'Euro':'€', 'Dollar':'$', 'Yen':'¥'}
+# clave=input('Escribir la divisa que desea usar: ')
+# print(diccionario.setdefault(clave,'La divisa no esta en el diccionario'))
+
+# diccionario={'Euro':'€', 'Dollar':'$', 'Yen':'¥'}
+# clave=input('Escribir la divisa que desea usar: ')
+# print(diccionario.setdefault(clave,'La divisa no esta en el diccionario'))
+
+
+
+# Escribir un programa que pregunte al usuario su nombre, edad, dirección y teléfono y lo guarde
+#  en un diccionario. Después debe mostrar por pantalla el mensaje <nombre> tiene <edad> años, 
+# vive en <dirección> y su número de teléfono es <teléfono>.
+
+# diccionario={}
+# lista=['nombre','apellido','celular']
+# for i in lista:
+#     print('la clave es', i)
+#     valor=input('Escriba el valor de la clave: ')
+#     diccionario.setdefault(i,valor)
+#     print('valor %s para la clave %s' % (valor,i))
+#     print()
+# print(diccionario)
+
+
+
+
