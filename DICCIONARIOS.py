f5b7aeecfcdfb71034d05e9ee22c69aa3943a480
elki pariona <elkiparionarojas@gmail.com>
2022-02-13 22:53:05 -0500

    final

--- DICCIONARIOS.py
+++ DICCIONARIOS.py
@@ -2,13 +2,13 @@
 # 'Yen':'¥'}, pregunte al usuario por una divisa y muestre su símbolo o un mensaje de aviso si 
 # la divisa no está en el diccionario.
 
-# diccionario={'Euro':'€', 'Dollar':'$', 'Yen':'¥'}
-# clave=input('Escribir la divisa que desea usar: ')
-# print(diccionario.setdefault(clave,'La divisa no esta en el diccionario'))
+diccionario={'Euro':'€', 'Dollar':'$', 'Yen':'¥'}
+clave=input('Escribir la divisa que desea usar: ')
+print(diccionario.setdefault(clave,'La divisa no esta en el diccionario'))
 
-# diccionario={'Euro':'€', 'Dollar':'$', 'Yen':'¥'}
-# clave=input('Escribir la divisa que desea usar: ')
-# print(diccionario.setdefault(clave,'La divisa no esta en el diccionario'))
+diccionario={'Euro':'€', 'Dollar':'$', 'Yen':'¥'}
+clave=input('Escribir la divisa que desea usar: ')
+print(diccionario.setdefault(clave,'La divisa no esta en el diccionario'))
 
 
 
@@ -16,15 +16,15 @@
 #  en un diccionario. Después debe mostrar por pantalla el mensaje <nombre> tiene <edad> años, 
 # vive en <dirección> y su número de teléfono es <teléfono>.
 
-# diccionario={}
-# lista=['nombre','apellido','celular']
-# for i in lista:
-#     print('la clave es', i)
-#     valor=input('Escriba el valor de la clave: ')
-#     diccionario.setdefault(i,valor)
-#     print('valor %s para la clave %s' % (valor,i))
-#     print()
-# print(diccionario)
+diccionario={}
+lista=['nombre','apellido','celular']
+for i in lista:
+    print('la clave es', i)
+    valor=input('Escriba el valor de la clave: ')
+    diccionario.setdefault(i,valor)
+    print('valor %s para la clave %s' % (valor,i))
+    print()
+print(diccionario)
 
 
 
@@ -33,44 +33,44 @@
 # ese número de kilos de fruta. Si la fruta no está en el diccionario debe mostrar un mensaje 
 # informando de ello.
 
-# claves=['platano','manzana','pera','naranja']
-# cantidad=[]
-# precio=[]
-# costo=[]
-# for i in claves:
-#     n=float(input('Escribir el precio de %s: ' % (i)))
-#     precio.append(n)
-# precio=list(precio)
-# print(precio)
-# for i in claves:
-#     m=float(input('Escribir la cantidad de %s: ' % (i)))
-#     cantidad.append(m)
-# cantidad=list(cantidad)
-# print(cantidad)
-# for i in range(len(cantidad)):
-#     costo_t=precio[i]*cantidad[i]
-#     costo.append(costo_t)
-# print(costo)
-# datos=zip(precio,cantidad,costo)
-# tupla=tuple(datos)
-# combi=zip(claves,tupla)
-# info=dict(combi)
-# print(info)
+claves=['platano','manzana','pera','naranja']
+cantidad=[]
+precio=[]
+costo=[]
+for i in claves:
+    n=float(input('Escribir el precio de %s: ' % (i)))
+    precio.append(n)
+precio=list(precio)
+print(precio)
+for i in claves:
+    m=float(input('Escribir la cantidad de %s: ' % (i)))
+    cantidad.append(m)
+cantidad=list(cantidad)
+print(cantidad)
+for i in range(len(cantidad)):
+    costo_t=precio[i]*cantidad[i]
+    costo.append(costo_t)
+print(costo)
+datos=zip(precio,cantidad,costo)
+tupla=tuple(datos)
+combi=zip(claves,tupla)
+info=dict(combi)
+print(info)
 
 
 
 # Escribir un programa que pregunte una fecha en formato dd/mm/aaaa y muestre por pantalla la 
 # misma fecha en formato dd de <mes> de aaaa donde <mes> es el nombre del mes.
 
-# mes={1:'enero',2:'febrero',3:'marzo',4:'abril',5:'mayo',6:'junio',7:'julio',8:'agosto',9:'setiembre',10:'octube',11:'noviembre',12:'diciembre'}
-# fecha=input('Escribe la fecha en formato(dia/mes/año): ')
-# ind1=fecha.find('/')
-# ind2=fecha.rfind('/')
-# d=fecha[0:ind1]
-# m1=fecha[ind1+1:ind2]
-# m2=mes.get(int(m1))
-# a=fecha[ind2+1:]
-# print('Hoy es el %s de %s de %s' % (d,m2,a))
+mes={1:'enero',2:'febrero',3:'marzo',4:'abril',5:'mayo',6:'junio',7:'julio',8:'agosto',9:'setiembre',10:'octube',11:'noviembre',12:'diciembre'}
+fecha=input('Escribe la fecha en formato(dia/mes/año): ')
+ind1=fecha.find('/')
+ind2=fecha.rfind('/')
+d=fecha[0:ind1]
+m1=fecha[ind1+1:ind2]
+m2=mes.get(int(m1))
+a=fecha[ind2+1:]
+print('Hoy es el %s de %s de %s' % (d,m2,a))
 
 
 
@@ -79,20 +79,20 @@
 # en el formato <asignatura> tiene <créditos> créditos, donde <asignatura> es cada una de las asignaturas del 
 # curso, y <créditos> son sus créditos. Al final debe mostrar también el número total de créditos del curso.
 
-# num=int(input('Cuantos cursos llevas?: '))
-# lista=[]
-# for i in range(num):
-#     m=input('Escribe los curso que deseas llevar: ')
-#     lista.append(m)
-# diccionario={}
-# suma=0
-# for i in lista:
-#     ñ=int(input('Escribe los creditos de %s: ' % (i)))
-#     suma+=ñ
-#     diccionario.setdefault(i,ñ)
-# for i in diccionario:
-#     print('El curso de %s tiene un total de %s créditos' % (i,diccionario.get(i)))
-# print('En total llevaras %s créditos' % (suma))
+num=int(input('Cuantos cursos llevas?: '))
+lista=[]
+for i in range(num):
+    m=input('Escribe los curso que deseas llevar: ')
+    lista.append(m)
+diccionario={}
+suma=0
+for i in lista:
+    ñ=int(input('Escribe los creditos de %s: ' % (i)))
+    suma+=ñ
+    diccionario.setdefault(i,ñ)
+for i in diccionario:
+    print('El curso de %s tiene un total de %s créditos' % (i,diccionario.get(i)))
+print('En total llevaras %s créditos' % (suma))
 
 
 
@@ -100,14 +100,14 @@
 # (por ejemplo nombre, edad, sexo, teléfono, correo electrónico, etc.) que se le pida al usuario. Cada vez que
 #  se añada un nuevo dato debe imprimirse el contenido del diccionario.
 
-# diccionario={}
-# continuar=True
-# while continuar:
-#     llave=input('Introduce el tipo de dato que deseas agregar: ')
-#     valor=input('Introduce el valor para el campo de %s : ' % (llave))
-#     diccionario[llave]=valor
-#     continuar=input('Deseas continuar?(si/no): ')=='si'
-# print(diccionario)
+diccionario={}
+continuar=True
+while continuar:
+    llave=input('Introduce el tipo de dato que deseas agregar: ')
+    valor=input('Introduce el valor para el campo de %s : ' % (llave))
+    diccionario[llave]=valor
+    continuar=input('Deseas continuar?(si/no): ')=='si'
+print(diccionario)
 
 
 
@@ -115,17 +115,17 @@
 # artículo y su precio y añadir el par al diccionario, hasta que el usuario decida terminar. Después se debe 
 # mostrar por pantalla la lista de la compra y el coste total, con el siguiente formato
 
-# diccionario={}
-# costo=0
-# continuar=True
-# while continuar:
-#     abarrote=input('Ingrese el producto que comprará: ')
-#     precio=int(input('Ingreso su precio: '))
-#     costo+=precio
-#     diccionario[abarrote]=precio
-#     continuar=input('Desea continuar(si/no): ')=='si'
-# print(diccionario)
-# print('La suma de todos los productos asciende a %s Soles'%(costo))
+diccionario={}
+costo=0
+continuar=True
+while continuar:
+    abarrote=input('Ingrese el producto que comprará: ')
+    precio=int(input('Ingreso su precio: '))
+    costo+=precio
+    diccionario[abarrote]=precio
+    continuar=input('Desea continuar(si/no): ')=='si'
+print(diccionario)
+print('La suma de todos los productos asciende a %s Soles'%(costo))
 
 
 
@@ -135,19 +135,19 @@
 # el diccionario para traducirla palabra a palabra. Si una palabra no está en el diccionario debe dejarla sin 
 # traducir.
 
-# diccionario={'hola':'hi','mis':'my','queridos':'dear','amigos':'friends'}
-# claves=diccionario.keys()
-# frase=input('Escribe una frase: ')
-# separados=frase.split(' ')
-# lista=[]
-# for i in separados:
-#     if i in claves:
-#         traducido=diccionario.get(i)
-#         lista.append(traducido)
-#     else:
-#         None
-# traducido=' '.join(lista)
-# print(traducido)
+diccionario={'hola':'hi','mis':'my','queridos':'dear','amigos':'friends'}
+claves=diccionario.keys()
+frase=input('Escribe una frase: ')
+separados=frase.split(' ')
+lista=[]
+for i in separados:
+    if i in claves:
+        traducido=diccionario.get(i)
+        lista.append(traducido)
+    else:
+        None
+traducido=' '.join(lista)
+print(traducido)
 
 
 
@@ -159,23 +159,23 @@
 # Después de cada operación el programa debe mostrar por pantalla la cantidad cobrada hasta el momento y 
 # la cantidad pendiente de cobro.
 
-# diccionario={}
-# suma=0
-# continuar=True
-# while continuar:
-#     operacion=input('Que operacion realizará(añadir/pagar): ')
-#     if operacion=='añadir':
-#         clave=input('Escriba el numero de factura que desea agregar: ')
-#         valor=int(input('Escriba el costo de la factura: '))
-#         diccionario[clave]=valor
-#         suma+=valor
-#     elif operacion=='pagar':
-#         clave=input('Escriba el numero de factura que desea eliminar: ')
-#         suma-=diccionario[clave]
-#         del diccionario[clave]
-#     continuar=input('Desea continuar(si/no): ')=='si'
-# print('El total de su deuda asciende a %s Soles' % (suma))
-# print(diccionario)
+diccionario={}
+suma=0
+continuar=True
+while continuar:
+    operacion=input('Que operacion realizará(añadir/pagar): ')
+    if operacion=='añadir':
+        clave=input('Escriba el numero de factura que desea agregar: ')
+        valor=int(input('Escriba el costo de la factura: '))
+        diccionario[clave]=valor
+        suma+=valor
+    elif operacion=='pagar':
+        clave=input('Escriba el numero de factura que desea eliminar: ')
+        suma-=diccionario[clave]
+        del diccionario[clave]
+    continuar=input('Desea continuar(si/no): ')=='si'
+print('El total de su deuda asciende a %s Soles' % (suma))
+print(diccionario)
 
 
 
@@ -194,54 +194,54 @@
 # 5) Terminar el programa.
 
 
-# diccionario_g={}
-# suma=0
-# continuar=True
-# lista=['nombre','dirección','teléfono','correo','preferente']
-# print('''Las operaciones que puede realizar son:
-# 1) Añadir elementos
-# 2) Eliminar facturas
-# 3) Mostrar cliente
-# 4) Listar todos los clientes
-# 5) Listar los clientes preferentes
-# 6) Terminar la operación''')
-# while continuar:
-#     operacion=input('Que operacion realizará(1/2/3/4/5/6): ')
-#     if operacion=='1':
-#         clave=input('Escriba el número de la factura que desea agregar: ')
-#         diccionario_e={}
-#         for i in lista:
-#             valor_e=input('Escribe el/la % s: ' % (i))
-#             diccionario_e.setdefault(i,valor_e)
-#         diccionario_g[clave]=diccionario_e
-#         continuar=input('Desea continuar(si/no): ')=='si'
-#     elif operacion=='2':
-#         clave=input('Escriba el numero de la factura que desea eliminar: ')
-#         del diccionario_g[clave]
-#         continuar=input('Desea continuar(si/no): ')=='si'
-#     elif operacion=='3':
-#         clave=input('Escriba el numero de la factura que desea ver: ')#0002
-#         dicci=diccionario_g.get(clave)
-#         print('El propietario de esta factura es %s' % (dicci.get('nombre')))
-#         continuar=input('Desea continuar(si/no): ')=='si'
-#     elif operacion=='4':
-#         lista1=diccionario_g.keys()
-#         for i in lista1:
-#             dic=diccionario_g[i]
-#             print('El cliente %s es dueño de la factura N° %s' % (dic.get('nombre'),i))
-#         continuar=input('Desea continuar(si/no): ')=='si'
-#     elif operacion=='5':
-#         lista1=diccionario_g.keys()
-#         for i in lista1:
-#             dic=diccionario_g[i]
-#             x=dic.get('preferente')
-#             if x=='si':
-#                 print('Es un cliente preferente %s ' % (dic['nombre']))
-#             else:
-#                 None
-#         continuar=input('Desea continuar(si/no): ')=='si'
-#     elif operacion=='6':
-#         continuar=False
+diccionario_g={}
+suma=0
+continuar=True
+lista=['nombre','dirección','teléfono','correo','preferente']
+print('''Las operaciones que puede realizar son:
+1) Añadir elementos
+2) Eliminar facturas
+3) Mostrar cliente
+4) Listar todos los clientes
+5) Listar los clientes preferentes
+6) Terminar la operación''')
+while continuar:
+    operacion=input('Que operacion realizará(1/2/3/4/5/6): ')
+    if operacion=='1':
+        clave=input('Escriba el número de la factura que desea agregar: ')
+        diccionario_e={}
+        for i in lista:
+            valor_e=input('Escribe el/la % s: ' % (i))
+            diccionario_e.setdefault(i,valor_e)
+        diccionario_g[clave]=diccionario_e
+        continuar=input('Desea continuar(si/no): ')=='si'
+    elif operacion=='2':
+        clave=input('Escriba el numero de la factura que desea eliminar: ')
+        del diccionario_g[clave]
+        continuar=input('Desea continuar(si/no): ')=='si'
+    elif operacion=='3':
+        clave=input('Escriba el numero de la factura que desea ver: ')#0002
+        dicci=diccionario_g.get(clave)
+        print('El propietario de esta factura es %s' % (dicci.get('nombre')))
+        continuar=input('Desea continuar(si/no): ')=='si'
+    elif operacion=='4':
+        lista1=diccionario_g.keys()
+        for i in lista1:
+            dic=diccionario_g[i]
+            print('El cliente %s es dueño de la factura N° %s' % (dic.get('nombre'),i))
+        continuar=input('Desea continuar(si/no): ')=='si'
+    elif operacion=='5':
+        lista1=diccionario_g.keys()
+        for i in lista1:
+            dic=diccionario_g[i]
+            x=dic.get('preferente')
+            if x=='si':
+                print('Es un cliente preferente %s ' % (dic['nombre']))
+            else:
+                None
+        continuar=input('Desea continuar(si/no): ')=='si'
+    elif operacion=='6':
+        continuar=False
 
 
 
@@ -257,13 +257,9 @@
 
 
 datos_clientes = "nif;nombre;email;teléfono;descuento\n01234567L;Luis González;luisgonzalez@mail.com;656343576;12.5\n71476342J;Macarena Ramírez;macarena@mail.com;692839321;8\n63823376M;Juan José Martínez;juanjo@mail.com;664888233;5.2\n98376547F;Carmen Sánchez;carmen@mail.com;667677855;15.7"
-
 listas_generales=datos_clientes.split('\n')
-
 lista_campos = listas_generales[0].split(';') 
-
 diccionario={}
-
 for i in listas_generales[1:]:
     valores={}
     lista_ind=i.split(';')
--- LISTAS_TUPLAS.py
+++ LISTAS_TUPLAS.py
@@ -4,50 +4,50 @@
 # sacado <nota> donde <asignatura> es cada una des las asignaturas de la lista y <nota> cada una 
 # de las correspondientes notas introducidas por el usuario.
 
-# cursos=input('Ingrese sus asignaturas: ')
-# notas=input('Ingrese su nota: ')
-# lista=[]
-# relacion=[]
-# while True:
-#     if cursos !='salir':
-#         lista.append(cursos)
-#         relacion.append(notas)
-#         cursos=input('Ingrese sus asignaturas: ')
-#         if cursos=='salir':
-#             print('Muchas gracias')
-#             break
-#         notas=input('Cuanto de nota has sacado: ')
-# for i in range(0,len(lista)):
-#     print('Yo estudio %s y me saque una nota de %s' % (lista[i],relacion[i]))
+cursos=input('Ingrese sus asignaturas: ')
+notas=input('Ingrese su nota: ')
+lista=[]
+relacion=[]
+while True:
+    if cursos !='salir':
+        lista.append(cursos)
+        relacion.append(notas)
+        cursos=input('Ingrese sus asignaturas: ')
+        if cursos=='salir':
+            print('Muchas gracias')
+            break
+        notas=input('Cuanto de nota has sacado: ')
+for i in range(0,len(lista)):
+    print('Yo estudio %s y me saque una nota de %s' % (lista[i],relacion[i]))
 
 
 
 # Escribir un programa que pregunte al usuario los números ganadores de la lotería primitiva, 
 # los almacene en una lista y los muestre por pantalla ordenados de menor a mayor.
 
-# ganadores=input('Ingrese el código de los ganadores: ')
-# lista=[]
-# while True:
-#     ganadores!='salir'
-#     lista.append(int(ganadores))
-#     ganadores=input('Ingrese el código de los ganadores: ')
-#     if ganadores=='salir':
-#         print('Muchas gracias ')
-#         break
-# print(lista)
-# lista.sort()
-# print(lista)
+ganadores=input('Ingrese el código de los ganadores: ')
+lista=[]
+while True:
+    ganadores!='salir'
+    lista.append(int(ganadores))
+    ganadores=input('Ingrese el código de los ganadores: ')
+    if ganadores=='salir':
+        print('Muchas gracias ')
+        break
+print(lista)
+lista.sort()
+print(lista)
 
 
 
 # Escribir un programa que almacene en una lista los números del 1 al 10 y los muestre por 
 # pantalla en orden inverso separados por comas.
 
-# lista=[]
-# for i in range(1,10+1):
-#     lista.append(i)
-# lista=lista[::-1]
-# print(lista)
+lista=[]
+for i in range(1,10+1):
+    lista.append(i)
+lista=lista[::-1]
+print(lista)
 
 
 
@@ -56,135 +56,137 @@
 # en cada asignatura y elimine de la lista las asignaturas aprobadas. Al final el programa debe 
 # mostrar por pantalla las asignaturas que el usuario tiene que repetir.
 
-# lista=[]
-# relacion=[]
-# decisión=input('Dese continuar: ')
-# while decisión=='si':
-#     cursos=input('Ingrese su asignatura: ')
-#     notas=int(input('Cuanto de nota ha sacado: '))
-#     if notas >= 10:
-#         decisión=input('Desea continuar: ')
-#         continue
-#     elif notas < 10:
-#         lista.append(cursos)
-#         relacion.append(notas)
-#         decisión=input('Desea continuar: ')
-# for i in range(0,len(lista)):
-#     print('Yo desaprobe %s porque tengo una nota de  %s' % (lista[i],relacion[i]))
+lista=[]
+relacion=[]
+decisión=input('Dese continuar: ')
+while decisión=='si':
+    cursos=input('Ingrese su asignatura: ')
+    notas=int(input('Cuanto de nota ha sacado: '))
+    if notas >= 10:
+        decisión=input('Desea continuar: ')
+        continue
+    elif notas < 10:
+        lista.append(cursos)
+        relacion.append(notas)
+        decisión=input('Desea continuar: ')
+for i in range(0,len(lista)):
+    print('Yo desaprobe %s porque tengo una nota de  %s' % (lista[i],relacion[i]))
 
 
 
 # Escribir un programa que almacene el abecedario en una lista, elimine de la lista las letras 
 # que ocupen posiciones múltiplos de 3, y muestre por pantalla la lista resultante.
 
-# letras=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','Ñ','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-# lista=[]
-# for i in range(0,len(letras)):
-#     if i%3==0:
-#         continue
-#     elif i%3!=0:
-#         lista.append(letras[i])
-# print(lista)
+letras=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','Ñ','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+lista=[]
+for i in range(0,len(letras)):
+    if i%3==0:
+        continue
+    elif i%3!=0:
+        lista.append(letras[i])
+print(lista)
 
-# palabra=input('Introduzca la palabra: ')
-# lista=[]
-# for i in palabra:
-#     lista.append(i)
-# if len(lista)%2==0:
-#     num=len(lista)/2
-#     num=int(num)
-#     p1=lista[:num]
-#     print(p1)
-#     p2=lista[num:]
-#     p2=p2[::-1]
-#     print(p2)
-#     if p1==p2:
-#         print('es una palabra palíndroma')
-#     else:
-#         print('No es una palabra palíndroma')
-# elif len(lista)%2!=0:
-#     num=(len(lista)-1)/2
-#     num=int(num)
-#     p1=lista[:num]
-#     print(p1)
-#     p2=lista[num+1:]
-#     p2=p2[::-1]
-#     print(p2)
-#     if p1==p2:
-#         print('es una palabra palíndroma')
-#     else:
-#         print('No es una palabra palíndroma')
+palabra=input('Introduzca la palabra: ')
+lista=[]
+for i in palabra:
+    lista.append(i)
+if len(lista)%2==0:
+    num=len(lista)/2
+    num=int(num)
+    p1=lista[:num]
+    print(p1)
+    p2=lista[num:]
+    p2=p2[::-1]
+    print(p2)
+    if p1==p2:
+        print('es una palabra palíndroma')
+    else:
+        print('No es una palabra palíndroma')
+elif len(lista)%2!=0:
+    num=(len(lista)-1)/2
+    num=int(num)
+    p1=lista[:num]
+    print(p1)
+    p2=lista[num+1:]
+    p2=p2[::-1]
+    print(p2)
+    if p1==p2:
+        print('es una palabra palíndroma')
+    else:
+        print('No es una palabra palíndroma')
 
 
 
 #Escribir un programa que pida al usuario una palabra y muestre por pantalla el número de veces 
 # que contiene cada vocal.
 
-# palabra=input('Escriba la palabra que desea: ')
-# palabra=palabra.lower()
-# lista=[]
-# for i in palabra:
-#     lista.append(i)
-# vocales=['a','e','i','o','u']
-# for i in vocales:
-#     print('El numero de veces que esta la vocal %s es de %s veces' % (i, lista.count(i)))
+palabra=input('Escriba la palabra que desea: ')
+palabra=palabra.lower()
+lista=[]
+for i in palabra:
+    lista.append(i)
+vocales=['a','e','i','o','u']
+for i in vocales:
+    print('El numero de veces que esta la vocal %s es de %s veces' % (i, lista.count(i)))
 
 
 
 #Escribir un programa que almacene en una lista los siguientes precios, 50, 75, 46, 22, 80, 
 # 65, 8, y muestre por pantalla el menor y el mayor de los precios.
 
-# lista=[50,75,46,22,80,65,8]
-# mayor=max(lista)
-# menor=min(lista)
-# print('El número mayor es %s y el número menor es %s' % (mayor, menor))
+lista=[50,75,46,22,80,65,8]
+mayor=max(lista)
+menor=min(lista)
+print('El número mayor es %s y el número menor es %s' % (mayor, menor))
 
 
 
 # Escribir un programa que almacene los vectores (1,2,3) y (-1,0,2) en dos listas y muestre 
 # por pantalla su producto escalar.
-# l1=[1,2,3]
-# l2=[-1,0,2]
-# lista=[]
-# for i in range(3):
-#     n=l1[i]*l2[i]
-#     lista.append(n)
-# print(lista)
+
+l1=[1,2,3]
+l2=[-1,0,2]
+lista=[]
+for i in range(3):
+    n=l1[i]*l2[i]
+    lista.append(n)
+print(lista)
 
 
 
 # Escribir un programa que almacene las matrices en una lista y muestre por pantalla su producto.
 # Para representar matrices mediante listas usar listas anidadas, representando cada vector fila 
 # en una lista.
-# A=(1,2,5,8,5)
-# B=list(A)
-# print(B)
+
+A=(1,2,5,8,5)
+B=list(A)
+print(B)
 
 
 
 #Escribir un programa que pregunte por una muestra de números, separados por comas, los guarde 
 # en una lista y muestre por pantalla su media y desviación típica.
 
-# lista=[]
-# x=True
-# while x:
-#     n=int(input('introduce un numero: '))
-#     lista.append(n)
-#     x=input('Desea continuar con la operación: ')
-#     if x=='si':
-#         pass
-#     elif x=='no':
-#         x=False
-# print(lista)
-# suma=0
-# contador=0
-# for i in lista:
-#     contador+=1
-#     suma=+i 
-# promedio=suma/contador
-# sumatoria=0
-# for i in lista:
-#     m=(i-promedio)**2
-#     sumatoria=+m
-# varianza=sumatoria/contador
-# print('El promedio es %s y la varianza es %s' % (promedio,varianza))
+lista=[]
+x=True
+while x:
+    n=int(input('introduce un numero: '))
+    lista.append(n)
+    x=input('Desea continuar con la operación: ')
+    if x=='si':
+        pass
+    elif x=='no':
+        x=False
+print(lista)
+suma=0
+contador=0
+for i in lista:
+    contador+=1
+    suma=+i 
+promedio=suma/contador
+sumatoria=0
+for i in lista:
+    m=(i-promedio)**2
+    sumatoria=+m
+varianza=sumatoria/contador
+print('El promedio es %s y la varianza es %s' % (promedio,varianza))
